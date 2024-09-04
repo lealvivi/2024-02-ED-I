f84d993d93dab6d4c03ac7dcb99cca3f3bd68192
coisacruel <115575302+lealvivi@users.noreply.github.com>
2024-09-04 10:38:40 -0300

    Update Ponto.h

--- TAD/Ponto/Ponto.h
+++ TAD/Ponto/Ponto.h
@@ -1,9 +1,10 @@
-#ifndef PONTO_H
-#define PONTO_H
-
+#ifndef PONTO_H //quando fizer um arq .h sempre
+#define PONTO_H  //escrever essas duas linhas
+//evitar duplicidade nas definicoes
 typedef struct _ponto Ponto;
+//_ponto esconde os dados pq nao mostar oq tem dentro dele
 
-Ponto* Ponto_create(float, float);
+Ponto* Ponto_create(float, float); //precisa usar ponteiro para pedir/alocar memoria. retornando endereco pro ponto
 void Ponto_destroy(Ponto*);
 void Ponto_print(Ponto*);
 void Ponto_get_x(Ponto*, float*);
