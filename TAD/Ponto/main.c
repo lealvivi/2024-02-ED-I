0a876edc8060ac4784292f7493224d1ef8a38882
coisacruel <115575302+lealvivi@users.noreply.github.com>
2024-09-04 10:45:46 -0300

    Update main.c

--- TAD/Ponto/main.c
+++ TAD/Ponto/main.c
@@ -1,7 +1,7 @@
 #include "Ponto.h"
 
 int main(){
-    Ponto *umPonto = Ponto_create(2.4566, 6.877654);
+    Ponto *umPonto = Ponto_create(2.4566, 6.877654);// criando umPonto  do tipo Ponto. aloca esses valores e 
     //A prox linha nao funciona, se descomentar.
     //printf("%f",umPonto->x);
     Ponto_print(umPonto);
